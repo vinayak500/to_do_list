6b33d4b9dac9ae0ac6f0aa93a6411b2abb92d76c
vinayak500 <vinayakshetty500@gmail.com>
2022-07-23 22:15:31 +0530

    added task delete functionality

--- index.js
+++ index.js
@@ -14,24 +14,35 @@ app.use(express.static('assests'));
 
 app.get('/', function(req,res)
 {
-      return res.render('home' ,{});
+    task.find({} , function(err , tasks){
+        if(err){
+            console.log('error in fetching data from db!');
+            return;
+        }
+
+        return res.render('home' ,{
+            title:"TO DO LIST",
+            task_list:  tasks
+        });
+    });  
 });
 
 
 app.post('/create-task' , function(req,res){
 
 
-    console.log(req.body);
+    // console.log(req.body);
+    // console.log(req.body.date);
       task.create({
         description: req.body.description ,
-        category: req.body.category
+        category: req.body.category ,
+        date: req.body.date
       } , function(err , newTask){
-        if(err)
-        {
+        if(err) {
             console.log('error in creating a task!');
             return;
         }
-        console.log(newTask);
+        // console.log(newTask);
         return res.redirect('/');
       });
 });
@@ -40,7 +51,17 @@ app.post('/create-task' , function(req,res){
 
 
 app.get('/delete-task' , function(req,res){
- 
+    let id = req.query.id;
+
+    task.findByIdAndDelete(id , function(err){
+       if(err) {
+        console.log('error in deleting an object from database!');
+        return;
+       }
+
+       console.log('deleted');
+       return res.redirect('back');
+    });
 });
 
 
